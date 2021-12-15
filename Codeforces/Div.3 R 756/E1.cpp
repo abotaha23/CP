48461805c6692f92c6d8e46b2103815dbd83f094
Ahmed Taha <70281704+ahmedtaha23@users.noreply.github.com>
2021-12-15 18:57:46 +0200

    Update E1.cpp

--- Codeforces/Div.3 R 756/E1.cpp
+++ Codeforces/Div.3 R 756/E1.cpp
@@ -20,33 +20,65 @@ typedef long long ll;
 
 using namespace std;
 
-//const int di[8]={0, 0, 1, -1, 1, -1, -1, 1}, dj[8]={1, -1, 0, 0, 1, -1, 1, -1};
+const int dx[8]={0, 0, 1, -1, 1, -1, -1, 1}, dy[8]={1, -1, 0, 0, 1, -1, 1, -1}, M = 1e9+7, M2 = 998244353;
 
-vector <int> adj[200010];
+vector <vector <int>> adj; vector <bool> fri; vector <int> nf, dis; bool flag;
+
+void dfs1(int cur, int par)
+{
+    if (cur != 1) dis[cur] = 1+dis[par];
+    if (fri[cur]) nf[cur] = 0;
+    for (auto &i : adj[cur]) {
+        if (i != par) {
+            dfs1(i, cur);
+            nf[cur] = min(nf[i]+1, nf[cur]);
+        }
+    }
+}
+
+void dfs2(int cur, int par)
+{
+    if (cur != 1 && adj[cur].size() == 1) {
+        flag = true;
+        return;
+    }
+    for (auto &i : adj[cur]) {
+        if (i != par && nf[i] > dis[i]) {
+            dfs2(i, cur);
+        }
+    }
+}
 
 void burn()
 {
-    int k, n; cin >> n >> k;
-    vector <bool> fri(k+1, false);
-    while(k--) {
-        int i; cin >> i;
-        fri[i] = true;
+    int n, k; cin >> n >> k;
+    flag = false;
+    fri.assign(n+1, false);
+    nf.assign(n+1, 1e9);
+    dis.assign(n+1, 0);
+    adj.assign(n+1, {});
+    for (int i = 0; i < k; i++) {
+        int in; cin >> in;
+        fri[in] = true;
     }
-    while(--n) {
+    for (int i = 1; i < n; i++) {
         int f, s;
         cin >> f >> s;
         adj[f].push_back(s);
         adj[s].push_back(f);
     }
+    dfs1(1, 1e9);
+    dfs2(1, 1e9);
+    cout << (flag ? "YES" : "NO");
 }
 
 int main()
 {
     Taha_on_da_code;
-    int t = 1;  cin >> t;
+    int t = 1; cin >> t;
     while(t--) {
         burn();
-        cout << endl;
+        cout << '\n';
     }
     return 0;
 }
